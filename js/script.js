e4b3eb33cc4f37c7906df35a57f3d7526b96b90a
Haris Shah <harisshah385@gmail.com>
2021-08-03 18:29:41 +0500

    about & services section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 // SCRIPT FOR MOBILE MENU
 
-
 const toggler = document.querySelector('.mobile-menu');
 const nav = document.querySelector('.navbar-list');
 const headerLogo = document.querySelector('.header-logo');
 
 toggler.addEventListener('click', () => {
-  nav.classList.toggle('active');
+	nav.classList.toggle('active');
 
-  if(nav.classList.contains('active')){
-    headerLogo.style.color = 'white';
-  } else {
-    headerLogo.style.color = '#121212';
-  }
+	if (nav.classList.contains('active')) {
+		headerLogo.style.color = 'white';
+	} else {
+		headerLogo.style.color = '#121212';
+	}
 });
